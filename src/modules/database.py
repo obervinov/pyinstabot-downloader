64f2e462951b8dcf9fab8f5e297eb64231b7c5f0
obervinov <obervinov@pm.me>
2024-03-18 20:29:34 +0400

    modified:   CHANGELOG.md modified:   src/modules/database.py

--- src/modules/database.py
+++ src/modules/database.py
@@ -240,7 +240,7 @@ class DatabaseClient:
                 'timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, '
                 'message_type VARCHAR(255) NOT NULL , '
                 'producer VARCHAR(255) NOT NULL , '
-                'message_content_base64 TEXT NOT NULL'
+                'message_content_base64 VARCHAR(1000) NOT NULL'
             )
         )
         # Create a table to keep users in the database
