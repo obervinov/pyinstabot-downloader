0e0c1b901c7e863b8cf71c61aed42d7312b52b2c
obervinov <bervinov.ob@gmail.com>
2024-09-11 13:06:44 +0400

    modified:   src/migrations/0004_vault_users_data.py

--- src/migrations/0004_vault_users_data.py
+++ src/migrations/0004_vault_users_data.py
@@ -6,7 +6,6 @@ https://github.com/obervinov/users-package/blob/v3.0.0/tests/postgres/tables.sql
 VERSION = '1.0'
 NAME = '0004_vault_users_data'
 
-import json
 
 def execute(obj):
     """
@@ -38,7 +37,7 @@ def execute(obj):
                 print(f"{NAME}: Founded {users_counter} users in users data")
 
                 for user in users:
-                    user_last_state = json.loads(obj.vault.kv2engine.read_secret(path=f"data/users/{user}"))
+                    user_last_state = obj.json.loads(obj.vault.kv2engine.read_secret(path=f"data/users/{user}"))
 
                     user_id = user
                     chat_id = 'unknown'
