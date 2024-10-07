b8d59c75691b4e841957438fde163de7da49aa86
obervinov <bervinov.ob@gmail.com>
2024-10-07 11:03:18 +0400

    modified:   src/modules/downloader.py

--- src/modules/downloader.py
+++ src/modules/downloader.py
@@ -71,6 +71,31 @@ class Downloader:
 
         log.info('[Downloader]: Creating a new instance...')
         self.client = Client()
+        settings = {
+            "uuids": {
+                "phone_id": "57d64c41-a916-3fa5-bd7a-3796c1dab122",
+                "uuid": "8aa373c6-f316-44d7-b49e-d74563f4a8f3",
+                "client_session_id": "6c296d0a-3534-4dce-b5aa-a6a6ab017443",
+                "advertising_id": "8dc88b76-dfbc-44dc-abbc-31a6f1d54b04",
+                "device_id": "android-e021b636049dc0e9"
+            },
+            "cookies":  {},  # set here your saved cookies
+            "last_login": 1596069420.0000145,
+            "device_settings": {
+                "cpu": "h1",
+                "dpi": "640dpi",
+                "model": "h1",
+                "device": "RS988",
+                "resolution": "1440x2392",
+                "app_version": "117.0.0.28.123",
+                "manufacturer": "LGE/lge",
+                "version_code": "168361634",
+                "android_release": "6.0.1",
+                "android_version": 23
+            },
+            "user_agent": "Instagram 117.0.0.28.123 Android (23/6.0.1; ...US; 168361634)"
+        }
+
         self.client.delay_range = [1, int(self.configuration['delay-requests'])]
         auth_status = self._login()
 
@@ -156,8 +181,6 @@ class Downloader:
                     path = self.client.photo_download(media_pk=media_pk, folder=path)
                 elif resource['media_type'] == 2 and media_info['product_type'] == 'feed':
                     path = self.client.video_download(media_pk=media_pk, folder=path)
-                elif resource['media_type'] == 2 and media_info['product_type'] == 'igtv':
-                    path = self.client.igtv_download(media_pk=media_pk, folder=path)
                 elif resource['media_type'] == 2 and media_info['product_type'] == 'clips':
                     path = self.client.clip_download(media_pk=media_pk, folder=path)
                 else:
