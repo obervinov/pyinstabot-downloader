0575a16b029c854fcf185f4ae0cd73e07bd47b20
obervinov <bervinov.ob@gmail.com>
2024-12-10 16:16:12 +0400

    modified:   src/bot.py

--- src/bot.py
+++ src/bot.py
@@ -422,14 +422,15 @@ def process_account(
     user = users.user_access_check(**requestor)
     if user.get('permissions', None) == users.user_status_allow:
         internal_user_id = None
-        exist_account = database.get_account_info(message.chat.id)
+        data = message_parser(message)
+        print(data)
+        exist_account = database.get_account_info(name=data['account_name'])
         if exist_account:
             log.info('[Bot]: account %s already exist in the database', message.chat.id)
             internal_user_id = exist_account[1]
         else:
             log.info('[Bot]: account %s does not exist in the database', message.chat.id)
-            data = message_parser(message)
-            account_info = downloader.get_account_info(username=data['account_name'])
+            account_info = downloader.get_account_info(username=data['account_name']).dict()
             database.add_account_info(data=account_info)
             internal_user_id = account_info['pk']
         posts_list = downloader.get_user_posts(user_id=internal_user_id)
